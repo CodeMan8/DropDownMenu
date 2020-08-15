9b59ec89c0931221847c9ce1401d80a9eb92aaf2
bartu <bartuakman8@gmail.com>
2020-08-15 21:36:46 +0300

    dropDown menu has been added.

--- DropDownMenu/ViewController.swift
+++ DropDownMenu/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import iOSDropDown
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dropDown: DropDown!
     override func viewDidLoad() {
         super.viewDidLoad()
+      dropDown.optionArray = ["Option 1", "Option 2", "Option 3"]
+      //Its Id Values and its optional
+     // dropDown.optionIds = [1,23,54,22]
+        dropDown.isSearchEnable  = false
+        dropDown.selectedRowColor = UIColor.gray
+        dropDown.checkMarkEnabled = false
+      // Image Array its optional
+       // dropDown.optionImageArray = []
+      // The the Closure returns Selected Index and String
+      dropDown.didSelect{(selectedText , index ,id) in
+        print("Selected String: \(selectedText) \n index: \(index)")
+      }
         // Do any additional setup after loading the view.
     }
 
 
-}
 
+
+}
---
+++ Pods/Target Support Files/Pods-DropDownMenuTests/Pods-DropDownMenuTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DropDownMenuTests : NSObject
+@end
+@implementation PodsDummy_Pods_DropDownMenuTests
+@end
